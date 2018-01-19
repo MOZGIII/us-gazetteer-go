befc15f805a39e7a007cc6c32f458e6a73c554f5
MOZGIII <mike-n@narod.ru>
2018-01-20 02:46:41 +0300

    Added comments at data/data.go

--- data/data.go
+++ data/data.go
@@ -4,5 +4,8 @@ import (
 	"github.com/MOZGIII/us-gazetteer-go/internal/generated"
 )
 
+// States holds all states.
 var States = generated.States
+
+// Counties holds all counties.
 var Counties = generated.Counties
