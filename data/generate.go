7059f9ee4d052242f70541eae42e2ab50cde5e54
MOZGIII <mike-n@narod.ru>
2018-01-20 01:49:41 +0300

    Added gofmt invocations for better output files at data/generate.go

--- data/generate.go
+++ data/generate.go
@@ -1,4 +1,6 @@
 package usgazetteer
 
 //go:generate go run ../internal/generate/main.go states ../sources/states.txt ../internal/generated/states.go
+//go:generate gofmt -w ../internal/generated/states.go
 //go:generate go run ../internal/generate/main.go counties ../sources/counties.txt ../internal/generated/counties.go
+//go:generate gofmt -w ../internal/generated/counties.go
